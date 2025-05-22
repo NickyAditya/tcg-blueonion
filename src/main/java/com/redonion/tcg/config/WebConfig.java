3784c16034e6b6fa2d33717afe1c10e35320dc3c
nicky  aditya <nicky.aditya@mhs.itenas.ac.id>
2025-05-22 23:04:57 +0700

    perbaiki error webconfig

--- src/main/java/com/redonion/tcg/config/WebConfig.java
+++ src/main/java/com/redonion/tcg/config/WebConfig.java
@@ -1,21 +1,22 @@
 package com.redonion.tcg.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        System.out.println("DEBUG: Configuring resource handlers");
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-        System.out.println("DEBUG: Resource handlers configured");
-    }
+        @Override
+        public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+                System.out.println("DEBUG: Configuring resource handlers");
+                registry.addResourceHandler("/static/**")
+                                .addResourceLocations("classpath:/static/");
+                registry.addResourceHandler("/css/**")
+                                .addResourceLocations("classpath:/static/");
+                registry.addResourceHandler("/**")
+                                .addResourceLocations("classpath:/static/");
+                System.out.println("DEBUG: Resource handlers configured");
+        }
 }
